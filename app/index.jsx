5eedc54bc0b487dbdf704c04361268dd6727155a
Sukhwinder <singhdevelops@gmail.com>
2024-10-15 10:02:59 +0100

    built tabs

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-pblack">Home</Text>
-            <Link href="/profile" style={{ color: 'blue' }}>Go to profile</Link>
+            <Link href="/home" style={{ color: 'blue' }}>Go to home</Link>
         </View>
     );
 }
